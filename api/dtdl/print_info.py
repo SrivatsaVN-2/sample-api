7e4f2ada8cd8719925e88bdb53ba5c54f2d31b8c
SrivatsaVN-2 <srivatsa.nalluraya@innowave.tech>
2026-03-04 16:05:12 +0530

    Update print_info.py

--- api/dtdl/print_info.py
+++ api/dtdl/print_info.py
@@ -1,1 +1,14 @@
 
+def print_info(interface):
+
+    config = interface.get_config()
+
+    if not config:
+        print("No configuration provided")
+        return
+
+    print("STB Configuration")
+    print("------------------")
+    print("Natco:", config.get("natco"))
+    print("Language:", config.get("language"))
+    print("Release Type:", config.get("release_type"))
